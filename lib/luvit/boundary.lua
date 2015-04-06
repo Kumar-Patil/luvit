5e8f503cc44046e87793b94ae1f7bb9a2bbd959f
Pearce Barry <pearce@boundary.com>
2015-04-06 14:19:43 -0500

    Local plugins param.json location support.
    For local plugins, we need the ability to place the param.json file in a more config-friendly location (e.g. /etc), decoupled from the plugin script location.

--- lib/luvit/boundary.lua
+++ lib/luvit/boundary.lua
@@ -5,14 +5,24 @@ local fs = require('fs')
 local json = require('json')
 
 local boundary = {argv = nil, param = nil}
-
--- import param.json data into a Lua table (boundary.param)
-local json_blob
-if (pcall(function () json_blob = fs.readFileSync("param.json") end)) then
-  pcall(function () boundary.param = json.parse(json_blob) end)
-end
+local plugin_basedir = "."
 
 -- create table of cmdline args (boundary.argv)
 boundary.argv = process.argv or nil
+if boundary.argv ~= nil then
+  -- if '--plguin-basedir' is present as the first arg, use its value as the path
+  -- to the param.json file and remove it from the arg table
+  if boundary.argv[1] == '--plugin-basedir' then
+    plugin_basedir = boundary.argv[2]
+    table.remove(boundary.argv, 1)
+    table.remove(boundary.argv, 1)
+  end
+end
+
+-- import param.json data into a Lua table (boundary.param)
+local json_blob
+if (pcall(function () json_blob = fs.readFileSync(plugin_basedir.."/param.json") end)) then
+  pcall(function () boundary.param = json.parse(json_blob) end)
+end
 
 return boundary
